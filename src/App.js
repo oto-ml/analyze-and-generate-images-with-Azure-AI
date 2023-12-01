9669d58c67878d4d812addbc9927ae4247ccb6fa
oto-ml <rmunoz1803@alumno.ipn.mx>
2023-11-30 23:31:33 -0600

    Adding header and buttons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Image Analyzer</h1>
+        <input type="text" placeholder="Enter image URL" />
+        <button>Analyze</button>
+        <button>Generate</button>
+      </header>
+    </div>
+  );
 }
 
 export default App;
