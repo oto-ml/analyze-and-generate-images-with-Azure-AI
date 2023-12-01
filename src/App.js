8740689fb49466642fa80568fbbc409904c7d882
oto-ml <rmunoz1803@alumno.ipn.mx>
2023-12-01 00:23:51 -0600

    Calling AnalyzeImage function

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
+import analyzeImage from './analyzeImage';
 
 function App() {
-  #write a title for the app
-  #put a box to input the image URL
-  #put a button to analyze the image
-  #put a button to generate the image
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Image Analyzer</h1>
         <input type="text" placeholder="Enter image URL" />
-        <button>Analyze</button>
+        <button onClick={analyzeImage}>Analyze</button>
         <button>Generate</button>
       </header>
     </div>
   );
 }
 
+
+
 export default App;
+
